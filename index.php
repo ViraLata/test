73123f8fe2b375c0261cfc7ac9f1fe40a3387bf9
Rafael Posada <rafaelposada@gmail.com>
2018-01-24 07:47:47 -0200

    Add files via upload
    First

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include("func.php");
+$url = "customers.json";
+$data = file_get_contents($url);
+$latdublin = 53.339428;
+$longdublin = -6.257664;
+$characters = json_decode($data);
+
+$a=array();
+
+
+foreach ($characters as $character) {
+	//Calculate de distance to the office of Dublin
+	$distance = distanceCalculation($latdublin, $longdublin, $character->latitude, $character->longitude);
+	if ($distance < 100) {
+		//save it
+		$a[$character->user_id] = $character->name;
+	}
+}
+
+
+ksort($a);
+echo "Iduser => Name <br>";
+foreach ($a as $clave => $valor) {
+    echo "{$clave} => {$valor} <br>";
+}
+?>
