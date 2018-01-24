6dd3ceef4432ce236532d620db692d71de258d27
Rafael Posada <rafaelposada@gmail.com>
2018-01-24 07:52:59 -0200

    Add files via upload

--- index.php
+++ index.php
@@ -8,20 +8,23 @@ $characters = json_decode($data);
 
 $a=array();
 
-
-foreach ($characters as $character) {
-	//Calculate de distance to the office of Dublin
-	$distance = distanceCalculation($latdublin, $longdublin, $character->latitude, $character->longitude);
-	if ($distance < 100) {
-		//save it
-		$a[$character->user_id] = $character->name;
+//check if is empty
+if ($characters) {
+	foreach ($characters as $character) {
+		//Calculate de distance to the office of Dublin
+		$distance = distanceCalculation($latdublin, $longdublin, $character->latitude, $character->longitude);
+		if ($distance < 100) {
+			//save it
+			$a[$character->user_id] = $character->name;
+		}
 	}
-}
 
 
-ksort($a);
-echo "Iduser => Name <br>";
-foreach ($a as $clave => $valor) {
-    echo "{$clave} => {$valor} <br>";
-}
+	ksort($a);
+	echo "Iduser => Name <br>";
+	foreach ($a as $clave => $valor) {
+	    echo "{$clave} => {$valor} <br>";
+	}
+} 
+
 ?>
